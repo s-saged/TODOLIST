7ec3ade82b23c0e14d2c12ba4f25b5b1c578aabd
s-saged <saged2000sallam@gmail.com>
2026-07-08 21:46:21 +0300

    adddd

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('fetch', (event) => {
-    // كود فارغ فقط لتخطي شروط جوجل كروم لتثبيت التطبيقات
+const CACHE_NAME = 'todo-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/icon.png',
+  '/manifest.json'
+];
+
+// تثبيت الـ Service Worker وحفظ الملفات
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// تشغيل التطبيق وجلب الملفات من الكاش لو مفيش نت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
